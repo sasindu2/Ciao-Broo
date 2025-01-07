ef8c2926c87b321826ce53487f360ecf96765bb4
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2025-01-07 22:00:30 +0530

    categories page

--- frontend/restaurant/src/components/Categories.jsx
+++ frontend/restaurant/src/components/Categories.jsx
@@ -1,22 +1,31 @@
-import React, { useState, useEffect } from "react";
-import "../components/categories.css"; 
-import categoriesData from "../data/categories.json"; 
+import React, { useState, useEffect } from 'react';
+import { categoryData } from '../data/categoryData';
+import './styles.css'
+
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    setCategories(categoriesData);
+    setCategories(categoryData.categories);
   }, []);
 
   return (
     <div className="categories-container">
-      <h2>Categories</h2>
+      <h2 className="categories-title">Categories</h2>
       <div className="categories-grid">
-        {categories.map((category, index) => (
-          <div key={index} className="category-card">
-            <img src={category.image} alt={category.name} />
-            <div className="category-name">{category.name}</div>
+        {categories.map((category) => (
+          <div key={category.id} className="category-card">
+            <div className="image-wrapper">
+              <img
+                src={category.image}
+                alt={category.name}
+                className="category-image"
+              />
+            </div>
+            <div className="category-overlay">
+              <span className="category-name">{category.name}</span>
+            </div>
           </div>
         ))}
       </div>
---
+++ frontend/restaurant/src/data/categoryData.js
@@ -0,0 +1,34 @@
+export const categoryData = {
+    categories: [
+      {
+        id: 1,
+        name: "Pizza",
+        image: "/src/assets/pizza.jpg"
+      },
+      {
+        id: 2,
+        name: "Bakery",
+        image: "/src/assets/bakary.jpg"
+      },
+      {
+        id: 3,
+        name: "Noodles",
+        image: "/src/assets/noodles.jpg"
+      },
+      {
+        id: 4,
+        name: "Pasta",
+        image: "/src/assets/pasta.jpg"
+      },
+      {
+        id: 5,
+        name: "Soup",
+        image: "/src/assets/soup.jpg"
+      },
+      {
+        id: 6,
+        name: "Korean",
+        image: "/src/assets/korean.jpg"
+      }
+    ]
+  };
