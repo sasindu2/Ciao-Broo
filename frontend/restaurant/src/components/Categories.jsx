39dc6c345abc8a68b9307d42e1a379d9c411ea49
Randika Dilshan <rdilshan077788@gmail.com>
2025-01-08 13:47:50 +0530

    catergory done

--- frontend/restaurant/src/components/Categories.jsx
+++ frontend/restaurant/src/components/Categories.jsx
@@ -1,31 +1,52 @@
 
 import React, { useState, useEffect } from 'react';
-import { categoryData } from '../data/categoryData';
 import './styles.css'
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    setCategories(categoryData.categories);
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/api/Category");
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
   }, []);
+
+
+  const handleCategoryClick = (id) => {
+    navigate(`/food/${id}`);
+  };
+
 
   return (
     <div className="categories-container">
       <h2 className="categories-title">Categories</h2>
       <div className="categories-grid">
         {categories.map((category) => (
-          <div key={category.id} className="category-card">
+          <div
+            key={category._id}
+            className="category-card"
+            onClick={() => handleCategoryClick(category._id)}
+            style={{ cursor: "pointer" }} 
+          >
             <div className="image-wrapper">
               <img
                 src={category.image}
-                alt={category.name}
+                alt={category.category_name}
                 className="category-image"
               />
             </div>
             <div className="category-overlay">
-              <span className="categories-name">{category.name}</span>
+              <span className="categories-name">{category.category_name}</span>
             </div>
           </div>
         ))}
